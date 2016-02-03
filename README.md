5443277aa1c6b991131bdcbfd919f49308b08290
Francois Hill (home win10) <francoishill11@gmail.com>
2016-02-03 05:33:13 +0200

    Refer to the README.md for progress. Currently we can install our app as a service by using the `NewServiceRunnerBuilder`

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # go-api-generator
 A generator for easily generating new backend (api) golang apps based on an input config (yaml) file
+
+## Goals
+
+The main of this repo is to reduce the amount of boiler-plate code. Using builder classes for elegance.
+
+So essentially this repo just abstracts as much as possible boilerplate into this repo and expose it via a simple api.
+
+## Features
+
+Easily create a installable service with this code:
+```
+package main
+
+import (
+    "github.com/zero-boilerplate/go-api-helpers/service"
+    "time"
+
+    service2 "github.com/ayufan/golang-kardianos-service"
+)
+
+type runHandler struct{}
+
+func (r *runHandler) Run(logger service2.Logger) {
+    for {
+        dur := 10 * time.Second
+        logger.Infof("I am a cool new app running, now sleeping for %s", dur.String())
+        time.Sleep(dur)
+    }
+}
+
+func main() {
+    r := &runHandler{}
+    service.NewServiceRunnerBuilder("TestSleepService", r).Run()
+}
+```
+
+To install as a service just build your app and call `BUILT_BINARY -service install`. Thanks to **https://github.com/ayufan/golang-kardianos-service** for making this so simple.
---
+++ service/runner_builder.go
@@ -0,0 +1,117 @@
+package service
+
+import (
+	"flag"
+	"github.com/ayufan/golang-kardianos-service"
+	"log"
+	"strings"
+)
+
+func NewServiceRunnerBuilder(serviceName string, runHandler RunHandler) ServiceRunnerBuilder {
+	if strings.TrimSpace(serviceName) == "" {
+		panic("The service name cannot be blank in NewServiceRunnerBuilder")
+	}
+
+	return &builder{
+		ServiceName:        serviceName,
+		ServiceDisplayName: serviceName, //default to name
+		ServiceDescription: serviceName, //default to name
+		ServiceUserName:    "",
+		RunHandler:         runHandler,
+	}
+}
+
+type ServiceRunnerBuilder interface {
+	WithServiceDisplayName(serviceDisplayName string) ServiceRunnerBuilder
+	WithServiceDescription(serviceDescription string) ServiceRunnerBuilder
+	WithServiceUserName(serviceUserName string) ServiceRunnerBuilder
+	WithServiceUserName_AsCurrentUser() ServiceRunnerBuilder
+
+	WithOnStopHandler(h OnStopHandler) ServiceRunnerBuilder
+
+	Run()
+}
+
+type builder struct {
+	ServiceName        string
+	ServiceDisplayName string
+	ServiceDescription string
+	ServiceUserName    string
+
+	RunHandler    RunHandler
+	OnStopHandler OnStopHandler
+}
+
+func (b *builder) WithServiceDisplayName(serviceDisplayName string) ServiceRunnerBuilder {
+	b.ServiceDisplayName = serviceDisplayName
+	return b
+}
+
+func (b *builder) WithServiceDescription(serviceDescription string) ServiceRunnerBuilder {
+	b.ServiceDescription = serviceDescription
+	return b
+}
+
+func (b *builder) WithServiceUserName(serviceUserName string) ServiceRunnerBuilder {
+	b.ServiceUserName = serviceUserName
+	return b
+}
+
+func (b *builder) WithServiceUserName_AsCurrentUser() ServiceRunnerBuilder {
+	return b.WithServiceUserName(getCurrentUserName())
+}
+
+func (b *builder) WithOnStopHandler(h OnStopHandler) ServiceRunnerBuilder {
+	b.OnStopHandler = h
+	return b
+}
+
+func (b *builder) Run() {
+	svcFlag := flag.String("service", "", "Control the system service.")
+	flag.Parse()
+
+	svcConfig := &service.Config{
+		Name:        b.ServiceName,
+		DisplayName: b.ServiceDisplayName,
+		Description: b.ServiceDescription,
+		UserName:    b.ServiceUserName,
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errs := make(chan error, 5)
+	logger, err := s.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prg.Logger = logger
+	prg.RunHandler = b.RunHandler
+	prg.OnStopHandler = b.OnStopHandler
+
+	go func() {
+		for {
+			err := <-errs
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
